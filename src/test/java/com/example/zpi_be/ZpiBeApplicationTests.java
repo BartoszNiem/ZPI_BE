c8dc10bc52d630eb15e76f9b89a06c1ac9467e8f
Bartek Niemczyk <bartek.niemczyk@interia.pl>
2023-04-17 22:35:04 +0200

    project_structure

---
+++ src/test/java/com/example/zpi_be/ZpiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zpi_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
