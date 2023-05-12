43e4e47e9a25cb661b10ece99e23a3dc74524001
Bartek Niemczyk <bartek.niemczyk@interia.pl>
2023-05-12 22:34:53 +0200

    posts controller

--- src/main/java/com/example/zpi_be/controller/PostController.java
+++ src/main/java/com/example/zpi_be/controller/PostController.java
@@ -24,12 +24,20 @@ public class PostController {
     List<Post> getAllPosts() {
         return postService.getAllPosts();
     }
-
     @PostMapping("/newPost")
     Post saveNewPost(@RequestBody Post post){
         postService.savePost(post);
         return post;
     }
 
-
+    @DeleteMapping("/delete/{post_id}")
+    Post deletePost(@PathVariable Long post_id){
+        Post dbPost = null;
+        try{
+            dbPost = postService.findPostById(post_id);
+        }catch (RuntimeException ex){
+            ex.printStackTrace();
+        }
+        return dbPost;
+    }
 }
